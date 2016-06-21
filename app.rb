5ce6ab6ea00ea3a9a35d9efcc117a467e43b720a
ziggles <ziggless1@gmail.com>
2016-06-21 11:27:30 -0400

    Fixed redirect from home

--- app.rb
+++ app.rb
@@ -6,6 +6,10 @@ class App < Sinatra::Base
     erb :home
   end
 
+  get '/home' do
+    redirect to('/')
+  end
+
   post '/subscribe' do
     @full_name = params[:full_name]
     @email = params[:email]
